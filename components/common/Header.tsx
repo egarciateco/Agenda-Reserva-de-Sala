3e06abe7873739656b82382ac9f70eaad037ca15
egarciateco <131554785+egarciateco@users.noreply.github.com>
2025-10-15 20:59:50 -0300

    feat: Improve PWA installability tooltip
    Update the tooltip for the PWA installation button to be more informative, specifically mentioning that the installation is not available in the development environment and will be enabled on a published website.

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -41,7 +41,7 @@ const Header: FC = () => {
                                 ? 'bg-blue-600 hover:bg-blue-700'
                                 : 'bg-gray-500 cursor-not-allowed text-gray-300'
                             }`}
-                            title={isPwaInstallable ? 'Instalar la aplicación en tu dispositivo' : 'La instalación no está disponible en este momento'}
+                            title={isPwaInstallable ? 'Instalar la aplicación en tu dispositivo' : 'La instalación no está disponible en este entorno de desarrollo. En un sitio web publicado, este botón se activará.'}
                         >
                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
